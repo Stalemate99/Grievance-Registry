5e6da6982e027f3a5d905f080dccb6b5da8a3520
mohanpierce99 <paying.rich6990@gmail.com>
2019-03-16 02:34:26 +0530

    Working version

--- home.js
+++ home.js
@@ -1,3 +1,86 @@
+ var db = firebase.firestore();
+var idofdoc;
+var address;
+var depart;
+        function initMap() {
+            var geocoder = new google.maps.Geocoder;
+            geocodeLatLng(geocoder);
+        }
+
+        function geocodeLatLng(geocoder) {
+            navigator.geolocation.getCurrentPosition(function (data) {
+                var latlng = {
+                    lat: parseFloat(data.coords.latitude),
+                    lng: parseFloat(data.coords.longitude)
+                };
+                geocoder.geocode({
+                    'location': latlng
+                },   function (results, status) {
+                    if (status === 'OK') {
+                        if (results[0]) {
+                          address=results[0].formatted_address;
+                            var regex = /\d{6}/g;
+                            var pincode = results[0].formatted_address.match(regex)[0];
+                            db.collection("districts").where("pincodes", "array-contains", pincode).get()
+                                .then(async (qs) => {
+                                    if (qs.size === 0) {
+                                        var district=await getDistrict(pincode);
+                                        district=district.PostOffice[0].District;
+                                        db.collection("districts").where("Name","==",district).get().then((qs)=>{
+                                            if(qs.size===0){
+                                                db.collection("districts").add({
+                                                    Name: district,
+                                                    pincodes:[pincode]
+                                                });
+                                            }else{
+                                                qs.forEach((doc)=>{
+                                                    console.log(doc);
+                                                    db.collection("districts").doc(doc.id).update({pincodes:firebase.firestore.FieldValue.arrayUnion(pincode)});
+                                                });
+                                            }
+                                        })
+                                    }
+                                      db.collection("districts").where("pincodes", "array-contains", pincode).get().then((qs)=>{
+                                        qs.forEach((doc)=>{
+                                          console.log("District recognized");
+                                          idofdoc=doc.id;
+                                       console.log(doc.id);
+                                        });
+                                      })
+                                    
+                                });
+                            getDistrict(pincode);
+
+                        } else {
+                            window.alert('No results found');
+                        }
+                    } else {
+                        window.alert('Geocoder failed due to: ' + status);
+                    }
+                });
+            });
+
+        }
+
+
+        function getDistrict(pincode) {
+            return new Promise((resolve,reject)=>{
+                fetch(`http://localhost:4000/reversegeocode/${pincode}`).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                resolve(data); 
+            }).catch(function (err) {
+                reject(err);
+            });
+            });
+          
+
+        }
+
+
+
+
+
 function sel(d){
     return document.querySelector(d);
 }
@@ -19,8 +102,7 @@ var file=myuploader.files[0];
     var task = storageRef.put(file);
 
     task.on('state_changed', function(snapshot){
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    
         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log('Upload is ' + progress + '% done');
         switch (snapshot.state) {
@@ -32,15 +114,69 @@ var file=myuploader.files[0];
             break;
         }
       }, function(error) {
-        // Handle unsuccessful uploads
+      console.log(error);
       }, function() {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+
         task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
           console.log('File available at', downloadURL);
+          httpreq('http://localhost:4000/visionai',JSON.stringify({path:downloadURL})).then((data)=>{
+
+            db.collection("complaints").add({
+              department:data,
+              uid:firebase.auth().currentUser.uid,
+              district:idofdoc,
+              description:sel("#messaget").value,
+              imageurl:downloadURL,
+              address,
+              status:"posted",
+              votes:1,
+              title:sel("#title").value,
+              date:firebase.firestore.FieldValue.serverTimestamp()
+            }).then((d)=>{
+              console.log("Successfully Recorded complaint");
+            });
+             
+          });
         });
       });
 });
+
+function httpreq(route,json){
+  var http = new Promise((res,rej)=>{
+      var xhttp = new XMLHttpRequest();
+      xhttp.open('POST',route,true);
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+                res(this.response);
+          }
+          else if(this.readyState===4 && this.status===404){
+              rej("error:file not found");
+          }else if(this.readyState===4 && this.status===401){
+              rej(this.response);
+          }
+      };
+          
+      xhttp.setRequestHeader('Content-Type','application/json; charset=UTF-8');
+      xhttp.send(json);
+  });
+
+  return http;
+}
+
+function postreq(path){
+  console.log(path);
+  return new Promise((res,rej)=>{
+    var obj={path}
+    fetch("http://localhost:3300/visionai",{
+      method:'post',
+      body:  JSON.stringify(obj)
+    }).then((response)=>response.json()).then((data)=>{
+      res(data);
+    }).catch((err)=>{
+      rej(err);
+    })
+  });
+}
 
 
 v.addEventListener("click",()=>{
